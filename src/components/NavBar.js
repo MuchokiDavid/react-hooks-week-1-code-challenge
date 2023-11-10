e346e1175ba4928afcc00b888dfe20bebfc31d44
dmmuchoki7 <dmmuchoki7@gmail.com>
2023-11-10 15:26:51 +0300

    updated styles

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ import React from 'react'
 function NavBar() {
   return (
     <div>
-        <nav className='h-12 bg-green-600 content-center'>
+        <nav className='h-14 bg-green-600 content-center mt-2 flex items-center justify-center'>
             <h1 className='text-white font-bold text-3xl'>Royal Bank of Flatiron</h1>
         </nav>
     </div>
--- src/components/Search.js
+++ src/components/Search.js
@@ -6,7 +6,7 @@ function Search() {
         <input
         type="text"
         name="search"
-        className="mt-6 block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-black focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+        className="mt-4 block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-black focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
         placeholder="Search..."      
         />
     </div>
